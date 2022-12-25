bdfe99dde9e9c73036140c8de616c7ca379b2773
benjaminlesieux <benjamin.lesieux@efrei.net>
2022-12-25 20:53:26 +0100

    Refactor findByType

--- src/drinks/controller/drinktypes.controller.ts
+++ src/drinks/controller/drinktypes.controller.ts
@@ -1,27 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { DrinksService } from '../service/drinks.service';
+import { DrinkType } from '../drinks.types';
+import { Drink } from '../entities/Drink';
 
 @Controller('drinktype')
 export class DrinktypesController {
   constructor(private service: DrinksService) {}
 
-  @Get('beers')
-  getBeers() {
-    return this.service.findByType('beer');
-  }
-
-  @Get('cocktails')
-  getCocktails() {
-    return this.service.findByType('cocktail');
-  }
-
-  @Get('mocktails')
-  getMocktails() {
-    return this.service.findByType('mocktail');
-  }
-
-  @Get('softs')
-  getSofts() {
-    return this.service.findByType('soft');
+  @Get(':type')
+  findByType(@Param('type') type: DrinkType): Promise<Drink[]> {
+    return this.service.findByType(type);
   }
 }
